2a09c40d590b71948b3c464e8b4e1bcf103bc45c
Javier García Escobedo <javier@iMac-de-Javier.local>
2015-01-22 21:57:53 +0100

    Control de estado de conexión, mejora de conversión de datos y comentarios

--- app/src/main/java/es/javiergarciaescobedo/ejemploaccesourl/MainActivity.java
+++ app/src/main/java/es/javiergarciaescobedo/ejemploaccesourl/MainActivity.java
@@ -1,47 +1,20 @@
 package es.javiergarciaescobedo.ejemploaccesourl;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.Toast;
-
-/**
- *     <uses-permission android:name="android.permission.INTERNET"/>
- */
+import android.support.v7.app.ActionBarActivity;
 
 public class MainActivity extends ActionBarActivity {
-
-    MainFragment mainFragment = null;   //Variable que hará referencia al Fragment que se añadirá
-                                        //  dentro de esta Activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Crea el Fragment y a continuación lo añade dentro de esta Activity
-        mainFragment = new MainFragment();
-
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-//                    .add(R.id.container, new MainFragment())
-                    .add(R.id.container, mainFragment)
+                    .add(R.id.container, new MainFragment())
                     .commit();
         }
-    }
-
-    //Método que recoge la pulsación del botón que se encuentra en pantalla
-    public void buttonPressed(View v) {
-        //Toda la funcionalidad se encuentra en el método accessUrl
-        mainFragment.accessUrl();
     }
 
 }
--- app/src/main/java/es/javiergarciaescobedo/ejemploaccesourl/MainFragment.java
+++ app/src/main/java/es/javiergarciaescobedo/ejemploaccesourl/MainFragment.java
@@ -1,5 +1,8 @@
 package es.javiergarciaescobedo.ejemploaccesourl;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -7,107 +10,159 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class MainFragment extends Fragment {
 
-    EditText editText = null;
+// Más información sobre la conexión HTTP desde Android en:
+// http://developer.android.com/training/basics/network-ops/connecting.html
+//
+// Requiere los siguientes permisos en AndroidManifest:
+// <uses-permission android:name="android.permission.INTERNET"/>
+// <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/>
+
+public class MainFragment extends Fragment
+        implements View.OnClickListener { // Este fragment debe detectar las pulsaciones de ratón
+    // en lugar de su Activity como sucedería por defecto.
+    // Requiere implementar el método onClick(View v)
+
+    // Etiqueta para los mensajes de depuración. Se usará el nombre de esta clase
+    private static final String DEBUG_TAG = MainFragment.class.getName();
+
+    private EditText editText1 = null;  // EditText donde se mostrará el contenido descargado
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        // Cargar el Fragment con la estructura del layout
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        editText = (EditText) rootView.findViewById(R.id.editText);
+
+        // Obtener referencias a los elementos de la pantalla
+        editText1 = (EditText) rootView.findViewById(R.id.editText1);
+        Button button1 = (Button) rootView.findViewById(R.id.button1);
+
+        // Indicar que esta misma clase se va encargar de gestionar los clics en el botón
+        // Para ello, se ha especificado en su cabecera que implementa la clase View.OnClickListener
+        button1.setOnClickListener(this);
+
         return rootView;
     }
 
-    //Método que inicia el proceso de llamada a la URL
-    public void accessUrl() {
-        Toast.makeText(getActivity(), "Enviando datos", Toast.LENGTH_LONG).show();
-        //Crea una instancia de la clase UrlGet que está declarada a continuación
-        UrlGet urlGet = new UrlGet();
-        //El método execute realiza el proceso de manera asíncrona, realizando implícitamente
-        //  una llamada al método doInBackground
-        urlGet.execute("http://www.rtve.es/rss/temas_espana.xml");
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            // Se ha pulsado el botón para iniciar la descarga
+            case R.id.button1:
+                // URL con el contenido que se va a descargar
+                String stringUrl = "http://www.rtve.es/rss/temas_espana.xml";
+
+                //Comprobar que hay conexión a Internet
+                ConnectivityManager connMgr = (ConnectivityManager)
+                        getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+                if (networkInfo != null && networkInfo.isConnected()) {
+                    // Hay conexión a Internet, así que se debe iniciar la descarga.
+                    // La llamada al método execute inicia la ejecución del método doInBackground
+                    // de la clase asíncrona (ejecutada en segundo plano) DownloadWebpageTask que
+                    // se encuentra declarada más adelante
+                    new DownloadWebpageTask().execute(stringUrl);
+                } else {
+                    // No hay conexión a Internet
+                    Toast.makeText(getActivity(), "No hay conexión a Internet", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
     }
 
-    //Clase que permite realizar la llamada a una URL. Requiere que sea una extensión de la clase
-    //  AsyncTask, ya que el proceso debe realizarse de manera asíncrona (se ejecuta en segundo
-    //  plano, continuando la ejecución de la aplicación sin esperar a que finalice este proceso)
-    public class UrlGet extends AsyncTask<String, Void, Void> {
+    // Se usa una clase heredada de AsyncTask para que se ejecute en segundo plano, de manera que
+    // la descarga de datos se realice sin que se quede parada la aplicación durante el tiempo que
+    // dure la descarga.
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
 
-        private boolean connectionError = false;
-        String answer = ""; //Recogerá los datos obtenidos desde la URL invocada
-
+        // Las operaciones que se desean ejecutar en segundo plano se deben indicar en el método
+        // doInBackground que recibirá como parámetro un array de Strings.
+        // Este método se ejecuta cuando se invoca al método execute. Esa llamada se ha hecho antes,
+        // y se le ha pasado por parámetro un único String con la URL del contenido a descargar.
+        // Por tanto en el primer elemento del array urls (parámetro recibido) estará esa dirección
+        // Retornará un String con el contenido de la web, o null en caso de error
         @Override
-        protected Void doInBackground(String... urls) {
-            Log.d(UrlGet.class.getName(), "Iniciando llamada a URL: " + urls[0]);
-            HttpURLConnection urlConnection = null;
-            InputStream inputStream = null;
+        protected String doInBackground(String... urls) {
             try {
-                URL url = new URL(urls[0]);
-
-                urlConnection = (HttpURLConnection) url.openConnection();
-                //Acortar el tiempo de intento de conexión y de recepción de datos
-                urlConnection.setConnectTimeout(10000);
-                urlConnection.setReadTimeout(10000);
-                //Recibir el contenido de la URL solicitada
-                inputStream = new BufferedInputStream(urlConnection.getInputStream());
-
-                //Leer el flujo de datos recibido convirtiéndolo a String para poder mostrarlo
-                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-                String inputLine;
-                try {
-                    while ((inputLine = br.readLine()) != null) {
-                        answer += inputLine + "\n";
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Log.d(UrlGet.class.getName(), answer);
-
+                // Intentar realizar la descarga de la URL recibida por parámetro, la cual se ha
+                // indicado en la llamada al método execute
+                return downloadUrl(urls[0]);
             } catch (IOException e) {
-                connectionError = true;
-                e.printStackTrace();
-            } finally {
-                urlConnection.disconnect();
-                if (inputStream != null) {
-                    try {
-                        inputStream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+                return null;
             }
-            return null;
         }
 
-        /**
-         * Se ejecuta de manera automática cuando haya finalizado el proceso de descarga
-         * de los datos desde la URL que ha sido solicitada
-         */
+        // El método onPostExecute se ejecuta automáticamente cuando ha terminado el proceso de la
+        // tarea que ha estado realizando en segundo plano el método doInBackground.
+        // Como parámetro recibe el String que el método doInBackground ha retornado, el cual
+        // almacena el contenido de la URL a la que se ha accedido
         @Override
-        protected void onPostExecute(Void result) {
-            if (connectionError) {
-                Toast.makeText(getActivity(), "No se ha podido realizar la conexión con el servidor", Toast.LENGTH_LONG).show();
-                ;
+        protected void onPostExecute(String result) {
+            if(result != null) {
+                // Mostrar en pantalla el contenido obtenido de la web
+                editText1.setText(result);
             } else {
-                Toast.makeText(getActivity(), "Datos enviados correctamente", Toast.LENGTH_LONG).show();
-                //En este ejemplo, se muestran todos los datos recibidos en un EditText
-                editText.setText(answer);
+                // No se ha podido realizar la descarga. Mostrar un mensaje de error
+                Toast.makeText(getActivity(),
+                        "No se ha podido realizar la descarga. La dirección podría ser incorrecta",
+                        Toast.LENGTH_LONG).show();
             }
         }
+    }
 
+    // Retorna el contenido de la URL que se indique por parámetro
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+
+        try {
+            URL url = new URL(myurl);
+
+            // Configurar la conexión acortando el tiempo de intento de conexión a 15 segundos y
+            // el de recepción de datos a 10 segundos. El método de conexión será GET y se permite
+            // la recepción de datos
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+
+            // Iniciar la conexión con la URL
+            Log.d(DEBUG_TAG, "Conectando con " + myurl);
+            conn.connect();
+
+            // Obtener el código de respuesta
+            int response = conn.getResponseCode();
+            Log.d(DEBUG_TAG, "El código de respuesta HTTP ha sido: " + response);
+
+            // Obtener el flujo de datos con el contenido de la URL
+            is = conn.getInputStream();
+
+            //Leer el flujo de datos recibido convirtiéndolo a String y retornarlo
+            String contentAsString = convertStreamToString(is);
+            return contentAsString;
+
+            // Asegurar que se queda cerrado el InputStream una vez utilizado
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    // Método auxiliar con truco para convertir InputStream a String
+    private String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
     }
 
 }
